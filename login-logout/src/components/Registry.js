5105cff9c52c0b89911cb42ec8d345d0dbef2ac3
Sathwic <sathwic97@gmail.com>
2022-10-02 13:28:39 +0530

    initialised react app

---
+++ login-logout/src/components/Registry.js
@@ -0,0 +1,13 @@
+import React, { useRef, useState, useEffect } from "react";
+import {
+  faCheck,
+  faTimes,
+  faInfoCircle,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+function Registry() {
+  return <div> Registry </div>;
+}
+
+export default Registry;
